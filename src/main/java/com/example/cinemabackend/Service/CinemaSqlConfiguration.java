85606393cd9e6647cdaaf2fc33f52fc8ca424142
EmiArn <emily.arnelid@gmail.om>
2022-04-04 13:38:16 +0200

    added sql configuration

---
+++ src/main/java/com/example/cinemabackend/Service/CinemaSqlConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.cinemabackend.Service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.example.cinemabackend")
+public class CinemaSqlConfiguration {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cinema");
+        dataSource.setUsername("root");
+
+        return dataSource;
+    }
+}
