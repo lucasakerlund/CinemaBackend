54a4a1e33ce5d9e23018033a36df4362a0a0017d
Lucas Åkerlund <lucas2001@hotmail.se>
2022-04-10 22:27:31 +0200

    added too many things

--- src/main/java/com/example/cinemabackend/Service/BookingsService.java
+++ src/main/java/com/example/cinemabackend/Service/BookingsService.java
@@ -2,9 +2,12 @@ package com.example.cinemabackend.service;
 
 import com.example.cinemabackend.api.BookingsController;
 import com.example.cinemabackend.dao.BookingsDao;
+import com.example.cinemabackend.model.Booking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingsService {
@@ -15,8 +18,16 @@ public class BookingsService {
         this.dao = dao;
     }
 
+    public List<Booking> getBookingsByCustomerId(int customerId){
+        return dao.getBookingsByCustomerId(customerId);
+    }
+
     public void addTicket(String customerId, String amountOfAdults, String amountOfChildren, String amountOfSeniors, String amountOfStudents, String scheduleId, String chairNumbers){
         dao.addTicket(customerId, amountOfAdults, amountOfChildren, amountOfSeniors, amountOfStudents, scheduleId, chairNumbers);
+    }
+
+    public boolean deleteBooking(int bookingId){
+        return dao.deleteBooking(bookingId);
     }
 
 }
